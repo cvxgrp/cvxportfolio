4e5c9a593cb5933e02c8ce28c844ac4c14b5876c
Enzo Busseti <enzo.busseti@me.com>
2018-04-21 13:54:04 -0700

    pre-commit script

--- pre_commit.sh
+++ pre_commit.sh
@@ -1,4 +1,11 @@
-## TODO repeat under python2 and python3
-## TODO later: move everything to docker 
+## TODO: move everything to docker 
+
+source activate cvxportfolio-testing
 nosetests
 flake8 --exclude="*/__init__.py" .
+source deactivate
+
+source activate cvxportfolio-testing-py2
+nosetests
+flake8 --exclude="*/__init__.py" .
+source deactivate
