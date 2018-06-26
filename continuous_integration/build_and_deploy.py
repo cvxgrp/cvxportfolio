f909ef016a68fe79d99090cedb5e6a738e8fb4f6
Enzo Busseti <enzo.busseti@me.com>
2018-06-26 14:20:12 -0700

    new deploy python script

---
+++ continuous_integration/build_and_deploy.py
@@ -0,0 +1,58 @@
+import os
+import subprocess
+import requests
+import json
+import cvxportfolio
+from distutils.version import StrictVersion
+
+# On Travis, you should set $CONDA_UPLOAD_TOKEN, $PYPI_USER, and $PYPI_PASSWORD
+
+CHANNEL = "cvxgrp"
+BUILDDIR = 'build'
+ARCH = json.load(subprocess.Popen("conda info --json", shell=True, 
+    stdout=subprocess.PIPE).stdout)['platform']
+LOCAL_VERSION = cvxportfolio.__version__
+
+def pypi_version(test=False):
+    if test:
+        url = "https://test.pypi.org/pypi/cvxportfolio/json"
+    else:
+        url = "https://pypi.org/pypi/cvxportfolio/json"
+    r = requests.get(url)
+    data = r.json()
+    versions = list(data["releases"].keys())
+    versions.sort(key=StrictVersion)
+    return versions[-1]
+
+PYPI_VERSION = pypi_version()
+
+print('local version:', LOCAL_VERSION)
+print('pypi version:', PYPI_VERSION)
+
+if (LOCAL_VERSION == PYPI_VERSION):
+    print ("Versions match, skipping build.")
+    exit(0)
+
+# conda
+if not (subprocess.call(["conda", BUILDDIR, "--output-folder=build", "conda-recipe/meta.yaml"]) == 0):
+    print('build failed')
+    exit(1)
+
+LAST_BUILD_NAME = [el for el in sorted(os.listdir('%s/%s'%(BUILDDIR,ARCH))) if el[:5] == 'cvxpo'][-1]
+
+for NEWARCH in ['osx-64', 'win-32', 'win-64', 'linux-32', 'linux-64']:
+    subprocess.call(["conda", "convert", 
+                     "--platform=%s"%NEWARCH, 
+                     "%s/%s/%s"%(BUILDDIR, ARCH, LAST_BUILD_NAME),
+                     "-o=%s"%BUILDDIR])
+    subprocess.call(["anaconda", "upload", "--force", 
+                     "--user=%s"%CHANNEL] +  
+                     ["--token=$CONDA_UPLOAD_TOKEN"] + 
+                     ["%s/%s/%s"%(BUILDDIR, NEWARCH, LAST_BUILD_NAME)])
+
+# pypi
+subprocess.call(["python", "setup.py", "sdist"])
+subprocess.call(["twine", "upload", "--skip-existing"] + 
+                 ["-u $PYPI_USER"] + 
+                 ["-p $PYPI_PASSWORD"] + 
+                 ["dist/*"])
