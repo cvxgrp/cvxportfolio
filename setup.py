4e6ad08bd841872271c58d94aee32c57e9ee38b9
Enzo Busseti <enzo.busseti@me.com>
2020-02-25 17:19:25 +0100

    v 0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cvxportfolio',
-    version='0.0.7',
+    version='0.0.8',
     author='Enzo Busseti and Steven Diamond',
     author_email='enzo.busseti@gmail.com, stevend2@stanford.edu',
     packages=['cvxportfolio',
