997c644e45e65ecd5f92097ba61b6eef1fcf4a0e
enzo.busseti <enzo.busseti@adia.ae>
2023-04-11 10:53:44 +0400

    sphinx napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = ['sphinx.ext.autodoc',
               'numpydoc',
               'sphinx.ext.mathjax',
               'sphinx.ext.githubpages',
+	          'sphinx.ext.napoleon',
               'myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
