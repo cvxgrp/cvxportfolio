c1583658e403e782b6242cc3aee799fba69219cc
Enzo Busseti <enzo.busseti@me.com>
2023-04-13 07:30:36 +0400

    version 0.1.1

--- cvxportfolio/__init__.py
+++ cvxportfolio/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 from .data import FredRate, Yfinance
 from .simulator import MarketSimulator
 from .result import SimulationResult
