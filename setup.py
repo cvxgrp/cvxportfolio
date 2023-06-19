70b59314427c6dcfba6ca08ae01a852c339baa88
Enzo Busseti <enzo.busseti@me.com>
2023-06-19 21:48:06 +0400

    version 0.3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cvxportfolio',
-    version='0.3.2',
+    version='0.3.3',
     author='Enzo Busseti, Stephen Boyd, Steven Diamond, BlackRock Inc.',
     maintainer='Enzo Busseti',
     author_email='enzo.busseti@gmail.com',
