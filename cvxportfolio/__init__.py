f3be8f641a441e9783d75d5de892e63a6e09f6de
Enzo Busseti <enzo.busseti@me.com>
2023-09-05 19:55:47 +0400

    version 0.4.7
    This release (mostly) contains fixes to github issues #106 and #107.
    The first solves a bug that happened when the user provides data
    (e.g., r_hat for ReturnsForecast) to be used in a backtest with varying
    universe, such as when there are IPOs in the backtest period. There was
    no logic to only select the relevant part (columns, in that case) of the
    user-provided data, and now there is, including multiple error checks.
    The second solves a bug in the accounting of short positions (the book
    is right, the code deviated from it). We changed CashReturn and
    (Stocks)HoldingCost, which have now a much clearer (and better
    documented) interface.

--- cvxportfolio/__init__.py
+++ cvxportfolio/__init__.py
@@ -17,7 +17,7 @@ This module only republishes the api of a selection of cvxportfolio modules.
 The __all__ attribute of each is used.
 """
 
-__version__ = "0.4.6"
+__version__ = "0.4.7"
 
 from .simulator import *
 from .policies import *
