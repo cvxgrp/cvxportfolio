8422634fdbcffe8370a317d243dccc1b3d12df95
Enzo Busseti <enzo.busseti@me.com>
2023-09-17 23:23:30 +0400

    version 0.4.10
    This revision contains various bug fixes, new better __repr__ (which is called when the user prints) and plot methods of BacktestResult, now both shown in the documentation in the Hello World Example page. Also, we include a first implementation of MarketSimulator.optimize_hyperparameters, which takes a policy defined with symbolic hyperparameters and optimizes their values for a user-specified back-test objective (by default Sharpe ratio). This is done very naïvely by simple greedy grid search of the hyper-parameter values.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='cvxportfolio',
-    version='0.4.9',
+    version='0.4.10',
     author='Enzo Busseti, Stephen Boyd, Steven Diamond, BlackRock Inc.',
     maintainer='Enzo Busseti',
     author_email='enzo.busseti@gmail.com',
