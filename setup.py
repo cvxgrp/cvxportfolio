132562c09663c3ed95f4bbf76079b89aac57486c
Enzo Busseti <enzo.busseti@me.com>
2023-10-05 21:28:00 +0400

    version 1.0.1
    This revision contains bug fixes and no new features.
    An inefficient operation in TransactionCost.simulate (that
    was introduced to comply with the new MarketData.serve
    protocol) has been reworked (if you noticed a slowdown
    in back-tests this probably fixes it). In SymbolData.update
    the data quality checks that have been introduced in 1.0
    have been adjusted to give less false positive error
    logs.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='cvxportfolio',
-    version='1.0.0',
+    version='1.0.1',
     author='Enzo Busseti, Stephen Boyd, Steven Diamond, BlackRock Inc.',
     maintainer='Enzo Busseti',
     author_email='enzo.busseti@gmail.com',
