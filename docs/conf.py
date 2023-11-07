2c050a8e711b06a1684a264255f6b5470783b24a
Enzo Busseti <enzo.busseti@me.com>
2023-11-07 18:41:05 +0400

    trying new readthedocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 
 # html_sidebars = {
 #    '**': [
