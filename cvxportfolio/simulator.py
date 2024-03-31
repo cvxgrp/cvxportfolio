f149fa3262fa9d2045a80fde2e071a9d30ac208c
Enzo Busseti <enzo.busseti@me.com>
2024-03-31 17:30:27 +0400

    test commit

--- cvxportfolio/simulator.py
+++ cvxportfolio/simulator.py
@@ -29,7 +29,7 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
-from multiprocess import Lock, Pool  # pylint: disable=no-name-in-module
+from multiprocessing import Lock, Pool  # pylint: disable=no-name-in-module
 
 from .cache import _load_cache, _mp_init, _store_cache
 from .costs import StocksHoldingCost, StocksTransactionCost
