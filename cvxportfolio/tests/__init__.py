ccffc12ee89a211052f786323f23a784405b67b5
Enzo Busseti <enzo.busseti@me.com>
2024-05-08 21:15:32 +0400

    removes other call to shutil.rmtree; was used in the testsuite. this completes the re-doing of unwanted merge (base_location=None, see GH #149), which was reverted in toto

--- cvxportfolio/tests/__init__.py
+++ cvxportfolio/tests/__init__.py
@@ -14,11 +14,10 @@
 """We make the tests a sub-package so we can ship them."""
 
 import logging
-import shutil
-import tempfile
 import time
 import unittest
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 import cvxpy as cp
 import numpy as np
@@ -34,8 +33,11 @@ class CvxportfolioTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Initialize test class."""
-        cls.datadir = Path(tempfile.mkdtemp())
-        logger.info('created %s', cls.datadir)
+        # gets deleted automatically when de-referenced
+        # pylint: disable=consider-using-with
+        cls._tempdir = TemporaryDirectory()
+        cls.datadir = Path(cls._tempdir.name)
+        logger.info('using TemporaryDirectory %s', cls.datadir)
 
         cls.sigma = pd.read_csv(
             Path(__file__).parent / "sigmas.csv",
@@ -73,8 +75,6 @@ class CvxportfolioTest(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         """Finalize test class."""
-        logger.info('removing %s', cls.datadir)
-        shutil.rmtree(cls.datadir)
         print('Timing report:')
         print(pd.Series(cls.timers))
 
