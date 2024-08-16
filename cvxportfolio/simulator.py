67acc54f45b6a769e185678cfbd5ce56ed262b23
Enzo Busseti <enzo.busseti@me.com>
2024-08-16 13:01:38 +0400

    Revert "Fixes #168;"
    This reverts commit d97ad88c500ae20032c54eac3223f2a4414bcd5c.
    
    On second thought, that wasn't a good idea. And breaks compatibility if
    one was using information_ratio as synonym of sharpe_ratio with CashBenchmark.
    Maybe add the benchmark name to the BackTestResult, and/or drop
    active returns/volatilities from the __repr__ when benchmark is CashBenchark.

--- cvxportfolio/simulator.py
+++ cvxportfolio/simulator.py
@@ -55,7 +55,6 @@ from multiprocess import Lock, Pool  # pylint: disable=no-name-in-module
 from .cache import _load_cache, _mp_init, _store_cache
 from .costs import StocksHoldingCost, StocksTransactionCost
 from .data import BASE_LOCATION, DownloadedMarketData, UserProvidedMarketData
-from .policies import AllCash
 from .result import BacktestResult
 
 PPY = 252
@@ -398,8 +397,7 @@ class MarketSimulator:
                     current_volumes=current_volumes,
                     current_prices=current_prices)
 
-                if hasattr(used_policy, 'benchmark') and not \
-                        isinstance(used_policy.benchmark, AllCash):
+                if hasattr(used_policy, 'benchmark'):
                     w_bm = used_policy.benchmark.current_value
                     bm_ret = w_bm @ current_returns
                 else:
