cc7f9a0ec7e535e84900e1446de35e124ce94692
Enzo Busseti <enzo.busseti@me.com>
2025-01-08 21:13:23 +0400

    version 1.4.1
    Maintenance revision. We removed all dependency version constraints
    to fully support the new Python 3.13 and Numpy 2.

--- cvxportfolio/__init__.py
+++ cvxportfolio/__init__.py
@@ -38,7 +38,7 @@ This module only republishes the api of a selection of cvxportfolio
 modules. The __all__ attribute of each is used.
 """
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 
 from .constraints import *
 from .costs import *
