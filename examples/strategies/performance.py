14d767c25a3bfe0ec3dc8af5e11ddf3a835a4ce4
Enzo Busseti <enzo.busseti@me.com>
2025-05-05 11:30:09 +0400

    [auto commit] ftse100_daily reconciliation & execution on 2025-05-05

--- examples/strategies/performance.py
+++ examples/strategies/performance.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 import cvxportfolio as cvx
 
-strats = ['dow30_daily', 'ndx100_daily', 'sp500_daily', 'ftse100_daily']
-etfs = ['DIA', 'QQQ', 'SPY', 'ISF.L']
+strats = ['dow30_daily', 'ndx100_daily', 'sp500_daily', 'ftse100_daily', 'marketneutral_daily']
+etfs = ['DIA', 'QQQ', 'SPY', 'ISF.L', None]
 
 for i, strat in enumerate(strats):
     print('\nSTRATEGY', strat)
@@ -13,13 +13,14 @@ for i, strat in enumerate(strats):
     v = h.sum(1)
     lrets = np.log(v).diff().shift(-1)
     print(np.exp(lrets)-1)
-    etf_ret = cvx.YahooFinance(etfs[i]).data['return'].loc[lrets.index]
-    print(etf_ret)
-    print(strat, 'mean logreturn', lrets.mean())
-    print(etfs[i], 'mean logreturn', np.log(1+etf_ret).mean())
     plt.figure()
     lrets.cumsum().plot(label=strat)
-    np.log(1+etf_ret).cumsum().plot(label=etfs[i])
+    if etfs[i] is not None:
+        etf_ret = cvx.YahooFinance(etfs[i]).data['return'].loc[lrets.index]
+        print(etf_ret)
+        print(etfs[i], 'mean logreturn', np.log(1+etf_ret).mean())
+        np.log(1+etf_ret).cumsum().plot(label=etfs[i])
+    print(strat, 'mean logreturn', lrets.mean())
     plt.legend()
 
 
